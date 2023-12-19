49a3a616901d8ca5596f495fe26c3c6e2fe6c4bf
will <wryennie@gmail.com>
2023-12-18 23:55:57 -0500

    added conversation record, so that bot can remember the conversation its having

--- bot.py
+++ bot.py
@@ -3,14 +3,17 @@ def read_transcribed_text(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
-def chat_with_bot(user_input, context, client, model="gpt-4", frequency_penalty=0, max_tokens=350):
+def chat_with_bot(user_input, context, client, conversation, model="gpt-4", frequency_penalty=0, max_tokens=350):
     """Creates a chat completion using the OpenAI API."""
+    # Append user's message to the conversation history
+    conversation.append({"role": "user", "content": user_input})
+
     # Preparing the messages for the API request
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "assistant", "content": context},
-        {"role": "user", "content": user_input}
+        {"role": "assistant", "content": context}
     ]
+    messages += conversation
 
     response = client.chat.completions.create(
         model=model,
@@ -25,10 +28,11 @@ def chat_with_bot(user_input, context, client, model="gpt-4", frequency_penalty=
 def start_chatbot(client):
     """Starts the chatbot interaction."""
     context = read_transcribed_text('transcribed_text.txt')
+    conversation = [] # Initialize the conversation history list
     print("Chatbot is ready to talk! Type 'quit' to exit.")
     while True:
         user_input = input("You: ")
         if user_input.lower() == 'quit':
             break
-        response = chat_with_bot(user_input, context, client)
+        response = chat_with_bot(user_input, context, client, conversation)
         print("Bot:", response)
